c0f3002a46f0915edd7c9ddd5d977d9b73344e0f
Marcos Milhomem <marcosapolo511@gmail.com>
2025-01-22 01:21:43 -0300

    react

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
+  const nome = "Marcos"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SayMyName nome={nome}/>
+      <Pessoa />
     </div>
   );
 }
