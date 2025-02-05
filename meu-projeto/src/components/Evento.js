f999a4c5f20881225fc1b66bc37fbb3a1c743d5c
Marcos Milhomem <marcosapolo511@gmail.com>
2025-02-05 12:04:20 -0300

    event

--- meu-projeto/src/components/Evento.js
+++ meu-projeto/src/components/Evento.js
@@ -3,7 +3,7 @@ import Button from "./evento/Button"
 function Evento({ numero }) {
 
     function meuEvento() {
-        console.log(`Opa, fui ativado numero ${numero ++}`)
+        alert (`Opa, fui ativado numero ${numero ++}`)
     }
 
     return(
