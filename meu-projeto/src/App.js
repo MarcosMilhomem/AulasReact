5a3995614430c39c0622e817efe3da21e15285df
Marcos Milhomem <marcosapolo511@gmail.com>
2025-02-15 00:07:32 -0300

    react

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -6,15 +6,18 @@ import Lista from './components/Lista';
 import Form from './components/Form';
 import Evento from './components/Evento';
 import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
 
 function App() {
   const nome = "Marcos"
   
+  const meusItens = ['React', 'Vue',  'Angular']
 
   return (
     <div className="App"> 
-      <h1>Renderização condicional</h1>
-      <Condicional/>
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens={[ ]}/>
     </div>
   );
 }
