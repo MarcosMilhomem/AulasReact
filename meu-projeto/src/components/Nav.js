6568925c492ae2860e8a638c89a389a5b11b2d1b
Marcos Milhomem <marcosapolo511@gmail.com>
2025-02-18 15:06:20 -0300

    Router

--- meu-projeto/src/components/Nav.js
+++ meu-projeto/src/components/Nav.js
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom"
+
 function Nav() {
     return (
         <div>
-            
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/empresa">Empresa</Link>
+                </li>
+                <li>
+                <Link to="/contato">Contato</Link>
+                </li>
+            </ul>
         </div>
     )
 }
